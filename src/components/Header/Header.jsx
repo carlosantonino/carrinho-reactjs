eabbc783ca52cfecf4e5315d53e1fa686bf60349
Carlos <72629502+carlosantonino@users.noreply.github.com>
2023-06-15 13:39:59 -0300

    Header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Header/Header.css';
+
+function Header() {
+  return (
+    <>
+      <header className="header">
+          Header
+      </header>
+    </>
+  );
+}
+
+export default Header;
